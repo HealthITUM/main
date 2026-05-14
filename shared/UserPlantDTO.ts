973aaa11a88f3c7d3c90cee1814ea9af70175b2d
d1ram <d1ram@mail.ru>
2026-05-14 23:15:23 +0200

    PLANT-40 PLANT-41 PLANT-44 PLANT-45 PLANT-46 Added SpeciesDTO, UserPlantDTO, RecipeDTO. Added DTO's for every endpoint. Changed UserDTO.

---
+++ shared/UserPlantDTO.ts
@@ -0,0 +1,34 @@
+import type { ISpecieDTO } from "./SpeciesDTO";
+
+export interface IUserPlantDTO { // Base class.
+    id: string;
+    plant_specie: ISpecieDTO;
+    name: string;
+    imageUrl: string; // <- OR FILE.
+}
+
+// GET my/plants/ OR GET my/plants/:id -> IUserPlant as a RESPONSE.
+
+// POST /my/plants/. Request.
+export interface IUserPlantCreateRequestDTO {
+    plant_specie: ISpecieDTO;
+    name: string;
+    image: File;
+}
+// PATCH /my/plants/:id. Request.
+export interface IUserPlantUpdateRequestDTO extends Partial<IUserPlantDTO>{}
+
+// SENSORS.
+export interface ISensorDTO { // Base class. GET my/plants/:id/sensors
+    id: string;
+    internal_chip_id: string;
+    last_seen: Date;
+    online: boolean;
+}
+
+// MEASUREMENT. GET my/plants/:id/measurement
+export interface IMeasurementDTO {
+    id: string;
+    values: JSON;
+    timestamp: Date;
+}
