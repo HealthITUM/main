96927e923c68ecb0598d82108426eb4f27fdc786
ales <satleralesmail@gmail.com>
2026-05-15 22:45:07 +0200

    PLANT-59 made a test script to make sure it works

--- backend/main/src/index.ts
+++ backend/main/src/index.ts
@@ -1,1 +1,31 @@
-console.log("hello world!");
+import dotenv from 'dotenv';
+import express from 'express';
+import type { Request, Response } from 'express';
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+dotenv.config();
+
+const app = express();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
+const PORT = 3001;
+
+app.use(express.json());
+
+// A test route to check DB connection
+app.get('/test-db', async (req: Request, res: Response) => {
+  try {
+    // This executes a simple query to see if the DB responds
+    await prisma.$queryRaw`SELECT 1`;
+    res.json({ status: 'Success', message: 'Connected to PostgreSQL!' });
+  } catch (error: any) {
+    res.status(500).json({ status: 'Error', message: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
