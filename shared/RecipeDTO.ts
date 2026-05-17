1d937f71f2d4ec285c6ce59a0c805d68945516ea
Tamara0666 <tamara.beber@student.um.si>
2026-05-17 20:08:21 +0200

    Changed RecipeDTO to fit first frontend - idea

--- shared/RecipeDTO.ts
+++ shared/RecipeDTO.ts
@@ -1,4 +1,5 @@
-import { IUserDTO } from "./UserDTO";
+import type { IUserDTO } from "./UserDTO";
+export type DietType = "none" | "vegan" | "vegetarian";
 
 export interface IRecipeDTO { // Base class. GET /recipes/. Response.
     id: string;
@@ -6,6 +7,11 @@ export interface IRecipeDTO { // Base class. GET /recipes/. Response.
     description: string;
     author: IUserDTO;
     ingredients: IIngredientHasRecipeDTO[];
+    prepTime: number;
+    cookTime: number;
+    dietType: DietType;
+    imageUrl?: string;
+    steps: string[];
 }
 export interface IIngredientHasRecipeDTO {
     id: string;
@@ -13,11 +19,16 @@ export interface IIngredientHasRecipeDTO {
     unit: string;
     amount: number;
 }
-
 // POST /recipes/
 export interface IRecipeCreateRequestDTO {
     name: string;
     description: string;
     authorId: string;
     ingredients: IIngredientHasRecipeDTO[];
+    prepTime: number;
+    cookTime: number;
+    plantIds: string[];
+    imageUrl?: string;
+    steps: string[];
+    dietType: DietType;
 }
