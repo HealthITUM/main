27cfd4edb1aa650ca26e88a6692c99ce961e91a2
d1ram <d1ram@mail.ru>
2026-05-18 23:59:15 +0200

    PLANT-72 Added service in docker-compose for MinIO initialization of buckets and service start. Added configuration for MinIO in storage.config.ts. Changed .env.example. Added few modules in package.json. Changed import of dotenv in prisma.config.ts.

--- backend/main/src/configs/storage.config.ts
+++ backend/main/src/configs/storage.config.ts
@@ -1,5 +1,40 @@
 import multer from 'multer';
 import path from 'path';
+import * as Minio from 'minio';
+
+export const minioClient = new Minio.Client({
+  endPoint: process.env.MINIO_ENDPOINT || 'minio',
+  port: parseInt(process.env.MINIO_PORT || '9000', 10),
+  useSSL: process.env.MINIO_USE_SSL === 'true', // dev = false
+  accessKey: process.env.MINIO_ROOT_USER || 'admin',
+  secretKey: process.env.MINIO_ROOT_PASSWORD || 'password',
+});
+
+export const BUCKETS = {
+  PLANTS: process.env.S3_BUCKET_PLANTS || 'plants',
+  RECIPES: process.env.S3_BUCKET_RECIPES || 'recipes',
+  SPECIES: process.env.S3_BUCKET_SPECIES || 'species',
+} as const;
+
+export const uploadFile = async (
+  bucket: typeof BUCKETS[keyof typeof BUCKETS],
+  objectName: string,
+  fileBuffer: Buffer,
+  mimeType: string
+): Promise<string> => {
+  try {
+    await minioClient.putObject(bucket, objectName, fileBuffer, fileBuffer.length, {
+      'Content-Type': mimeType,
+    });
+
+    // Creating direct link for an image. For browser link should be localhost and not minio.
+    const publicHost = process.env.MINIO_PUBLIC_URL || 'http://localhost:9000';
+    return `${publicHost}/${bucket}/${objectName}`;
+  } catch (error) {
+    console.error(`[MinIO Error] Error while loading an image ${objectName} into the ${bucket}:`, error);
+    throw new Error('Failed to upload file to storage');
+  }
+};
 
 const storage = multer.memoryStorage(); // RAM-storage.
 
--- backend/main/src/index.ts
+++ backend/main/src/index.ts
@@ -6,6 +6,8 @@ import userRouter from "./routes/UserRoute.js";
 import userPlantRouter from "./routes/UserPlantRoute.js";
 import recipeRouter from "./routes/RecipeRoute.js";
 import plantSpecieRouter from "./routes/PlantSpecieRoute.js";
+import { uploadMiddleware, uploadFile, BUCKETS } from './configs/storage.config.js';
+import path from 'node:path';
 
 dotenv.config();
 
@@ -27,4 +29,38 @@ const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT} port.`);
+});
+
+// test route
+app.post('/api/upload-test', (req, res) => {
+  // 1. Вызываем multer-мидлвар
+  uploadMiddleware(req, res, async (err) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err.message });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ success: false, error: 'Файл не выбран' });
+    }
+
+    try {
+      const ext = path.extname(req.file.originalname);
+      const objectName = `${Date.now()}${ext}`;
+
+      const fileUrl = await uploadFile(
+        BUCKETS.PLANTS,
+        objectName,
+        req.file.buffer,
+        req.file.mimetype
+      );
+
+      return res.status(200).json({
+        success: true,
+        message: 'Файл успешно загружен в MinIO!',
+        url: fileUrl,
+      });
+    } catch (uploadError: any) {
+      return res.status(500).json({ success: false, error: uploadError.message });
+    }
+  });
 });
