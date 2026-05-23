4c7a126bae496f10941621e4416d28a2e2e75e4c
ales <satleralesmail@gmail.com>
2026-05-23 21:40:04 +0200

    Added Recipe repository

--- backend/main/src/repositories/RecipeRepository.ts
+++ backend/main/src/repositories/RecipeRepository.ts
@@ -1,25 +1,119 @@
 import type { IRecipeDTO } from "@project/shared";
 import type { IRecipeCreateModel } from "../models/Recipe.js";
+import type { IIngredientHasRecipeDTO } from "@project/shared"
+import { prisma } from "../lib/prisma.js";
 
 export class RecipeRepository {
     async getRecipes(amount? : number) : Promise<IRecipeDTO[] | null> {
-        // TODO
-        return null;
+        const result = await prisma.recipes.findMany({
+            take: 10,
+            include: {
+                ingredients: true
+            }
+        });
+
+        if (!result) {
+            return null;
+        }
+        
+        const recipes: IRecipeDTO[] = result.map((result) => {
+            const ingredients: IIngredientHasRecipeDTO[] = (result.ingredients || []).map((item) => {
+                return {
+                    id: Number(item?.id),
+                    name: String(item?.name),
+                    unit: String(item?.unit),
+                    amount: Number(item?.amount)
+                };
+            });
+
+            return {
+                id: Number(result?.id),
+                name: String(result?.dishName),
+                description: String(result?.description),
+                authorId: Number(result?.fkUserId),
+                imageUrl: String(result?.imagePath),
+                ingredients: ingredients
+            };
+        });
+
+        return recipes;
     }
 
     async getById(id : number) : Promise<IRecipeDTO | null> {
-        // TODO
-        return null;
+        const result = await prisma.recipes.findUnique({
+            where: {id: id},
+            include: { ingredients: true },
+        });
+
+        if (!result) {
+            return null;
+        }
+
+        const ingredients: IIngredientHasRecipeDTO[] = (result.ingredients || []).map((item) => {
+            return {
+                id: Number(item?.id),
+                name: String(item?.name),
+                unit: String(item?.unit),
+                amount: Number(item?.amount)
+            };
+        });
+
+        const recipe: IRecipeDTO = {
+            id: Number(result?.id),
+            name: String(result?.dishName),
+            description: String(result?.description),
+            authorId: Number(result?.fkUserId),
+            imageUrl: String(result?.imagePath),
+            ingredients: ingredients
+        };
+        
+        return recipe;
     }
 
     async create(data : IRecipeCreateModel) : Promise<boolean> {
-        // TODO
-        return false;
+        try {
+            const newRecipe = await prisma.recipes.create({
+                data: {
+                    dishName: data.name,
+                    description: data.description,
+                    imagePath: data.imageUrl,
+                    author: {
+                        connect: { id: data.authorId }
+                    },
+                    ingredients: {
+                        connect: data.ingredients.map((ingredient) => ({
+                            id: ingredient.id
+                        }))
+                    }
+                }
+            });
+    
+            return !!newRecipe;
+
+        } catch (error) {
+            console.error("Failed to create recipe:", error);
+            return false;
+        }
     }
 
-    async delete(recipeId : number, userId : number) : Promise<boolean> {
-        // TODO
-        return false;
+    async delete(recipeId : number, userId: number) : Promise<boolean> {
+        try {
+            const deleteResult = await prisma.recipes.deleteMany({
+              where: {
+                id: recipeId,
+                fkUserId: userId,
+              },
+            });
+
+            if (deleteResult.count === 0) {
+                return false;
+            }
+            return true;
+            
+        } catch (error) {
+            console.error("Failed to delete recipe:", error);
+            return false;
+        }
     }
 }
 
