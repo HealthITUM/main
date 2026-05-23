4856f5f4a4404c2145d855d791cbdb74616f6c48
ales <satleralesmail@gmail.com>
2026-05-23 21:39:16 +0200

    Added Measurements repository

--- backend/main/src/repositories/MeasurementRepository.ts
+++ backend/main/src/repositories/MeasurementRepository.ts
@@ -1,20 +1,66 @@
 import type { IMeasurementDTO } from "@project/shared";
+import { prisma } from "../lib/prisma.js";
 import type { IMeasurementCreateModel } from "../models/Measurement.js";
 
 export class MeasurementRepository {
     async getMeasurements(plantId : number, amount? : number) : Promise<IMeasurementDTO[] | null>{
-        // TODO
-        return null;
+        const result = await prisma.measurements.findMany({
+            where: {fkUserPlantsId: plantId},
+            take: amount? Number(amount): 5,
+            orderBy: { timestamp: 'desc'}
+        });
+
+        if (!result) {
+            return null;
+        }
+     
+        const measurements: IMeasurementDTO[] = result.map((result) => {
+            return {
+                id: Number(result?.id),
+                values: (result?.values as Record<string, any>) ?? {},
+                timestamp: result?.timestamp ? new Date(result.timestamp) : new Date(),
+                plantId: result?.fkUserPlantsId 
+            };
+        });
+        
+        return measurements;
     }
     
     async getById(id : number) : Promise<IMeasurementDTO | null> {
-        // TODO
-        return null;
+        const result = await prisma.measurements.findUnique({
+            where: {id: id}
+        });
+
+        if (!result) {
+            return null;
+        }
+
+        const measurement: IMeasurementDTO = {
+            id: Number(result?.id),
+            values: (result?.values as Record<string, any>) ?? {},
+            timestamp: result?.timestamp ? new Date(result.timestamp) : new Date(),
+            plantId: Number(result?.fkUserPlantsId)
+        };
+        
+        return measurement;
     }
 
-    async create(data : IMeasurementCreateModel) : Promise<boolean> {
-        // TODO
-        return false;
+    async create(data: IMeasurementCreateModel) {
+        try {
+            const newMeasurements = await prisma.measurements.create({
+                data: {
+                    fkUserPlantsId: Number(data.plantId),
+                    values: data.values as any,
+                    timestamp: data.timestamp
+                }
+            });
+    
+            return !!newMeasurements;
+
+        } catch (error) {
+            console.error("Failed to create plant species:", error);
+            return false;
+        }
     }
 }
 
