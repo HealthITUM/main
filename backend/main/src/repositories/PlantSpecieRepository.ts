d9e738c4805c41450e8b0c4a84b05235b96823f7
ales <satleralesmail@gmail.com>
2026-05-23 21:39:42 +0200

    Added Plant Species repository

--- backend/main/src/repositories/PlantSpecieRepository.ts
+++ backend/main/src/repositories/PlantSpecieRepository.ts
@@ -1,14 +1,66 @@
+import { prisma } from "../lib/prisma.js";
+
 import type { ISpecieDTO } from "@project/shared";
+import type { ISpecieCreateModel } from "../models/Species.ts";
 
 export class PlantSpecieRepository {
-    async getSpecies(amount? : number) : Promise<ISpecieDTO[] | null>{
-        // TODO
-        return null;
+    async getSpecies() : Promise<ISpecieDTO[] | null>{
+        const result = await prisma.plantSpecies.findMany();
+
+        if (!result) {
+            return null;
+        }
+
+        const plantSpecies: ISpecieDTO[] = result.map((result) => {
+            return {
+                id: Number(result?.id),
+                name: String(result?.name),
+                description: String(result?.description),
+                ideal_values: (result?.idealValues as Record<string, any>) ?? {},
+                imageUrl: String(result?.imagePath)
+            };
+        });
+
+        return plantSpecies;
     }
     
     async getById(id : number) : Promise<ISpecieDTO | null> {
-        // TODO
-        return null;
+        const result = await prisma.plantSpecies.findUnique({
+            where: {id: id}
+        });
+
+        if (!result) {
+            return null;
+        }
+
+        const plant_specie: ISpecieDTO = {
+            id: Number(result?.id),
+            name: String(result?.name),
+            description: String(result?.description),
+            ideal_values: (result?.idealValues as Record<string, any>) ?? {},
+            imageUrl: String(result?.imagePath)
+        }
+        
+        return plant_specie;
+    }
+
+    async create(data: ISpecieCreateModel){
+        try {
+            const newSpecies = await prisma.plantSpecies.create({
+                data: {
+                    name: data.name,
+                    description: data.description,
+                    idealValues: data.ideal_values as any,
+                    imagePath: data.imageUrl
+                }
+            });
+    
+            return !!newSpecies;
+
+        } catch (error) {
+            console.error("Failed to create plant species:", error);
+            return false;
+        }
     }
 }
 
