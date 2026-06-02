9d1c0443ff280ab8b3da790364ab9be7cf65a06e
Tamara0666 <tamara.beber@student.um.si>
2026-06-02 13:20:24 +0200

    Fixed some connections with backend to frontend

--- backend/main/src/controllers/UserPlantController.ts
+++ backend/main/src/controllers/UserPlantController.ts
@@ -48,6 +48,7 @@ class UserPlantController {
             return res.status(200).json(userPlant);
         }
         catch (error){
+            console.log("Error in getById:", error);
             return res.status(500).json({ message : "Error on the server." });
         }
     }
